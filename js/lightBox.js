077913bb9345e7a13e9fc8e354a74ff64a619a0d
fujiman94 <tomofujikawa.94@gmail.com>
2023-04-29 12:53:06 -0400

    culminacion del efecto lightBox

--- js/lightBox.js
+++ js/lightBox.js
@@ -1,23 +1,82 @@
+(function () {
+
 //Objeto con las propiedades del efecto LightBox
 var propLightbox = {
 
     imgContainer: document.getElementsByClassName('lightbox'),
-    imagen: null
+    imagen: null,
+    imagenSrc: null,
+    cuerpoDom: document.getElementsByTagName('body')[0],
+    lightbox_container: null,
+    modal: null,
+    cerrarModal: null,
+    animacion: 'fade'
+
 }
+
 
 //Objeto con los Metodos del efecto lightBox
 var metLightbox = {
     inicio: function() {
-        for (var i = 0; propLightbox.imgContainer.length; i++) {
-
+        for (var i = 0; i < propLightbox.imgContainer.length; i++) {
             propLightbox.imgContainer[i].addEventListener('click', metLightbox.capturaImagen);
         }
     },
-    
-    captureImagen: function(){
-        propLightbox.imagen = this;
+
+
+    //guardar la imagen que se selecciono
+    capturaImagen: function(){
+        propLightbox.imagen = this; // aqui se guarda la informacion del elemento donde el SCSS ubica la imagen.
         metLightbox.lightbox(propLightbox.imagen);
+    },
+
+    //agregar direccion de la imagen que agregamos al SCSS
+    lightbox: function(imagen) {   // imagen = propLightbox.imagen, que es el elemento donde se ubica la imagen
+
+        propLightbox.imagenSrc = window.getComputedStyle(imagen, null).backgroundImage.slice(5, -2);
+
+        propLightbox.cuerpoDom.appendChild(document.createElement('DIV')).setAttribute('id', 'lightbox_container');
+
+        propLightbox.lightbox_container = document.getElementById('lightbox_container');
+
+        propLightbox.lightbox_container.style.width = '100%';
+        propLightbox.lightbox_container.style.height = '100%';
+        propLightbox.lightbox_container.style.position = 'fixed';
+        propLightbox.lightbox_container.style.zIndex = '1000';
+        propLightbox.lightbox_container.style.background = 'rgba(0,0,0,0.8)';
+        propLightbox.lightbox_container.style.top = '0';
+        propLightbox.lightbox_container.style.left = '0';
+
+        propLightbox.lightbox_container.appendChild(document.createElement('DIV')).setAttribute('id', 'modal');
+
+        propLightbox.modal = document.getElementById('modal');
+
+        propLightbox.modal.style.height = '100%';
+
+        propLightbox.modal.appendChild(document.createElement('IMG')).setAttribute('src', propLightbox.imagenSrc);
+
+        propLightbox.modal.getElementsByTagName('img')[0].setAttribute('class', 'imagen-modal');
+
+        if (propLightbox.animacion == 'fade') {
+            document.getElementsByClassName('imagen-modal')[0].style.opacity = 0;
+
+            setTimeout(function (){
+                document.getElementsByClassName('imagen-modal')[0].style.opacity = 1
+            }, 50);
+        }
+
+        propLightbox.modal.innerHTML += '<i class="fa-solid fa-x" id="cerrar_modal"></i>'
+
+        propLightbox.cerrarModal = document.getElementById('cerrar_modal');
+        propLightbox.cerrarModal.addEventListener('click', metLightbox.cerrarModal);
+    },
+
+    cerrarModal: function() {
+        propLightbox.cuerpoDom.removeChild(propLightbox.lightbox_container);
     }
+    
 }
 
 metLightbox.inicio();
+
+}())
