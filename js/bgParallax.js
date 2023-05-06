5fd115e28c99b05ab2ecb6aa7471b52d849286cc
fujiman94 <tomofujikawa.94@gmail.com>
2023-05-06 12:59:54 -0400

    finalizacion del parallax

--- js/bgParallax.js
+++ js/bgParallax.js
@@ -1,7 +1,10 @@
+(function(){
 // Objeto con propiedades de Parallax 
 
 var propParallax = {
     seccion: document.querySelector('.parallax'),
+    recorrido: null,
+    limite: null,
 }
 
 // objeto con Metodos de Parallax
@@ -13,9 +16,20 @@ var metParallax = {
     },
 
     scrollParallax: function() {
-    
+        propParallax.recorrido = window.pageYOffset;
+        propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+
+        if ( propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite) {
+
+            propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px';
+
+        }   else {
+            propParallax.seccion.style.backgroundPositionY = 0;
+        }
     }
 
 }
 
 metParallax.inicio();
+
+}())
